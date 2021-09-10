0464b7d976f486629e7326748d5411ecb93dde19
hbh <sout1217@naver.com>
2021-09-10 17:42:33 +0900

    코로나 누적 감염수 차트

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -9,7 +9,7 @@
 
 <div id="modal-section"></div>
 <div id="root">
-    <main class="covid-main pt-5">
+    <main class="covid-main py-5">
 
         <jsp:include page="header.jsp"/>
 
@@ -25,28 +25,28 @@
                         확진자
                     </h5>
                     <span class="infected-count text-weight-bold error--text">로딩중</span>
-                    <span class="text-weight-bold radius error--light-1 error--text text--11px">100↓</span>
+                    <span class="infected-count-cha text-weight-bold radius error--light-1 error--text text--11px">로딩중↓</span>
                 </div>
                 <div class="flex flex-direction gap-y-2">
                     <h5>
                         사망자
                     </h5>
                     <span class="deaths-count text-weight-bold">로딩중</span>
-                    <span class="text-weight-bold radius error--light-1 error--text text--11px">200↓</span>
+                    <span class="deaths-count-cha text-weight-bold radius error--light-1 error--text text--11px">로딩중↓</span>
                 </div>
                 <div class="flex flex-direction gap-y-2">
                     <h5>
                         완치자
                     </h5>
                     <span class="healing-count text-weight-bold success--text">로딩중</span>
-                    <span class="text-weight-bold radius error--light-1 error--text text--11px">300↓</span>
+                    <span class="release-from-quarantine-count-cha text-weight-bold radius error--light-1 error--text text--11px">로딩중↓</span>
                 </div>
                 <div class="flex flex-direction gap-y-2">
                     <h5>
                         검사자
                     </h5>
                     <span class="examine-count text-weight-bold primary--text">로딩중</span>
-                    <span class="text-weight-bold radius primary--light-1 primary--text text--11px">400↑</span>
+                    <span class="examine-count-cha text-weight-bold radius primary--light-1 primary--text text--11px">로딩중↑</span>
                 </div>
             </div>
         </div>
@@ -63,24 +63,28 @@
                 <div class="flex flex-direction text--11px">
                     <div class="my-2 flex justify-content-space-between gap-x-1">
                         <span>vs 어제</span>
-                        <span class="text-weight-bold radius primary--light-1 primary--text">116↓</span>
+                        <span class="text-weight-bold radius primary--light-1 primary--text">50↓</span>
                     </div>
                     <div class="my-2 flex justify-content-space-between gap-x-1">
-                        <span>vs 2주전 </span>
-                        <span class="text-weight-bold radius error--light-1 error--text">116↓</span>
+                        <span>vs 1주전 </span>
+                        <span class="text-weight-bold radius error--light-1 error--text">10↑</span>
                     </div>
                 </div>
                 <div class="flex flex-direction text--11px">
                     <div class="my-2 flex justify-content-space-between gap-x-1">
-                        <span>vs 어제</span>
-                        <span class="text-weight-bold radius primary--light-1 primary--text">116↓</span>
+                        <span>vs 2주전</span>
+                        <span class="text-weight-bold radius primary--light-1 primary--text">50↓</span>
                     </div>
                     <div class="my-2 flex justify-content-space-between gap-x-1">
-                        <span>vs 2주전 </span>
-                        <span class="text-weight-bold radius error--light-1 error--text">116↓</span>
+                        <span>vs 1달전 </span>
+                        <span class="text-weight-bold radius error--light-1 error--text">10↑</span>
                     </div>
                 </div>
             </div>
+        </div>
+
+        <div class="card py-2 mb-4-2">
+            <div id="chart"></div>
         </div>
 
     </main>
--- WebContent/static/index.js
+++ WebContent/static/index.js
@@ -74,14 +74,114 @@ function updateCovidStatus(obj) {
     var healingCount = document.querySelector('.healing-count');
     var examineCount = document.querySelector('.examine-count');
 
+    var infectedCountCha = document.querySelector('.infected-count-cha')
+    var deathsCountCha = document.querySelector('.deaths-count-cha')
+    var releaseFromQuarantineCountCha = document.querySelector('.release-from-quarantine-count-cha')
+    var examineCountCha = document.querySelector('.examine-count-cha')
+
     updateDate.innerText = obj['date'] + ' 업데이트';
-    infectedCount.innerText = obj['infected_count'];
-    deathsCount.innerText = obj['deaths_count'];
-    healingCount.innerText = obj['healing_count'];
-    examineCount.innerText = obj['examine_count'];
+    infectedCount.innerText = obj['infected_count'].toLocaleString();
+    deathsCount.innerText = obj['deaths_count'].toLocaleString();
+    healingCount.innerText = obj['healing_count'].toLocaleString();
+    examineCount.innerText = obj['examine_count'].toLocaleString();
+
+
+    // 증가인지 감소인지 판단여부가 필요해보입니다
+    infectedCountCha.innerText = obj['infected_count_cha'].toLocaleString() + ' ?';
+    deathsCountCha.innerText = obj['deaths_count_cha'].toLocaleString() + ' ?';
+    releaseFromQuarantineCountCha.innerText = obj['release_from_quarantine_count_cha'].toLocaleString() + ' ?';
+    examineCountCha.innerText = obj['examine_count_cha'].toLocaleString() + ' ?';
+}
+
+function getCovidStatusMonth() {
+    var xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+            var covidStatus = JSON.parse(xhttp.responseText)
+            console.log(covidStatus)
+
+            drawMap(covidStatus)
+        }
+    };
+    xhttp.open('GET', 'http://localhost:8080/api/covid_status_month.jsp')
+    xhttp.send()
+}
+
+
+function drawMap(obj) {
+
+    var options = {
+        series: [
+            {
+                name: '누적 확진자',
+                data: obj.map(covid => covid['infected_count'])
+            }
+        ],
+        chart: {
+            height: 350,
+            type: 'area',
+            locales: [{
+                "name": "ko",
+                "options": {
+                    "months": ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+                    "shortMonths": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+                    "days": ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"],
+                    "shortDays": ["일", "월", "화", "수", "목", "금", "토"],
+                    "toolbar": {
+                        "exportToSVG": "다운로드 SVG",
+                        "exportToPNG": "다운로드 PNG",
+                        "exportToCSV": "다운로드 CSV",
+                        "menu": "메뉴",
+                        "selection": "Selection",
+                        "selectionZoom": "선택 영역 확대",
+                        "zoomIn": "확대",
+                        "zoomOut": "축소",
+                        "pan": "Panning",
+                        "reset": "초기화"
+                    }
+                }
+            }],
+            defaultLocale: "ko"
+        },
+        title: {
+            text: '누적 감염수',
+            align: 'left'
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'smooth'
+        },
+        xaxis: {
+            type: 'datetime',
+            categories: obj.map(covid => new Date(covid['date']).getTime()),
+            labels: {
+                format: 'MM.dd'
+            },
+        },
+        yaxis: {
+            labels: {
+                formatter: function (value) {
+                    return value.toLocaleString();
+                }
+            },
+        },
+        tooltip: {
+            x: {
+                format: 'yy/MM/dd'
+            },
+        },
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
 }
 
 window.onload = function () {
     getCovidStatus()
-};
+    getCovidStatusMonth()
+}
+
+
 
